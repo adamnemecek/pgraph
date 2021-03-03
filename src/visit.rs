84c67f810654082dfef39c8dcec621dcd22a9fe3
adamnemecek <adamnemecek@gmail.com>
2021-03-03 11:58:02 -0800

    visitmap

--- src/visit.rs
+++ src/visit.rs
@@ -1,9 +1,25 @@
+use generational_arena::TypedIndex;
+
 pub trait VisitMap<N> {
     /// Mark `a` as visited.
     ///
     /// Return **true** if this is the first visit, false otherwise.
-    fn visit(&mut self, a: N) -> bool;
+    fn visit(&mut self, a: TypedIndex<N>) -> bool;
 
     /// Return whether `a` has been visited before.
-    fn is_visited(&self, a: &N) -> bool;
+    fn is_visited(&self, a: TypedIndex<N>) -> bool;
+}
+
+impl<T> VisitMap<T> for fixedbitset::FixedBitSet
+// where
+    // Ix: IndexType,
+{
+    fn visit(&mut self, x: TypedIndex<T>) -> bool {
+        // !self.put(x.index())
+        todo!()
+    }
+    fn is_visited(&self, x: TypedIndex<T>) -> bool {
+        // self.contains(x.index())
+        todo!()
+    }
 }
