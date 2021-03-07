e597f3b755df41b297cf400bb8d8365954906c4b
adamnemecek <adamnemecek@gmail.com>
2021-03-07 09:35:55 -0800

    removing dead code

--- src/outgoing_neighbors.rs
+++ src/outgoing_neighbors.rs
@@ -26,32 +26,5 @@ impl<'a, N, E> Iterator for OutgoingNeighbors<'a, N, E> {
         } else {
             None
         }
-        // if let Some(edge) = self.next.outgoing {
-        //     println!("next {:?}", edge.debug());
-        //     let node = self.inner[edge].to();
-        //     self.next = self.inner[node].next;
-        //     Some(node)
-        // } else {
-        //     None
-        // }
     }
 }
-
-// match self.edges.get(self.next[0].index()) {
-//     None => {}
-//     Some(edge) => {
-//         self.next[0] = edge.next[0];
-//         return Some(edge.nodes[1]);
-//     }
-// }
-// // Then incoming edges
-// // For an "undirected" iterator (traverse both incoming
-// // and outgoing edge lists), make sure we don't double
-// // count selfloops by skipping them in the incoming list.
-// while let Some(edge) = self.edges.get(self.next[1].index()) {
-//     self.next[1] = edge.next[1];
-//     if edge.nodes[0] != self.skip_start {
-//         return Some(edge.nodes[0]);
-//     }
-// }
-// None
